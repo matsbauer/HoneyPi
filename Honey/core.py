c2d4106c4db84938e7e32c1c23a0e55f69b8418e
matsbauer <mats.bauer@icloud.com>
2018-01-25 08:02:16 +0000

    Skeleton

---
+++ Honey/core.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.4
+# encoding: utf-8
+# creator: matsbauer
+# date created: 24.01.2018
+
+
+if __name__ == "__main__":
+    print("nothing")
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "Honey",
+    version = "0.0.1",
+    author = "Mats Bauer",
+    author_email = "contact@airlibi.com",
+    description = ("Honey is a package for Python that let's you automatically notify your honey when your working later - as you only need to fix this one last siiiiimple bug!"),
+    license = "MIT",
+    keywords = "python automation honey notice",
+    url = "https://github.com/matsbauer/Honey",
+    packages=['Honey'],
+    #long_description=open('readme-pypi.rst').read(),
+    #install_requires=['bs4', 'beautifulsoup4']
+)
